df1e596eb254e23ba635ed47d20de15c8e7d563a
qw2548er <qw2548er@users.noreply.github.com>
2026-07-21 14:49:05 +0000

    Initial commit: FoldCraftLauncher project

---
+++ FCL/build.gradle.kts
@@ -0,0 +1,204 @@
+import com.android.build.api.variant.FilterConfiguration.FilterType.ABI
+import com.android.build.gradle.tasks.MergeSourceSetFolders
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.20"
+}
+
+android {
+    namespace = "com.tungsten.fcl"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    var localProperty: Properties? = null
+    if (file("${rootDir}/local.properties").exists()) {
+        localProperty = Properties()
+        file("${rootDir}/local.properties").inputStream().use { localProperty.load(it) }
+    }
+    val pwd = System.getenv("FCL_KEYSTORE_PASSWORD") ?: localProperty?.getProperty("pwd")
+    val curseApiKey = System.getenv("CURSE_API_KEY") ?: localProperty?.getProperty("curse.api.key")
+    val oauthApiKey = System.getenv("OAUTH_API_KEY") ?: localProperty?.getProperty("oauth.api.key")
+    if (localProperty != null && localProperty.getProperty("arch", "all") == "arm64")
+        System.setProperty("arch", "arm64")
+
+    signingConfigs {
+        create("FCLKey") {
+            storeFile = file("../key-store.jks")
+            storePassword = pwd
+            keyAlias = "FCL-Key"
+            keyPassword = pwd
+        }
+        create("FCLDebugKey") {
+            storeFile = file("../debug-key.jks")
+            storePassword = "FCL-Debug"
+            keyAlias = "FCL-Debug"
+            keyPassword = "FCL-Debug"
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.tungsten.fcl"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1319
+        versionName = "1.3.1.9"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("FCLKey")
+        }
+        create("fordebug") {
+            initWith(getByName("debug"))
+            applicationIdSuffix = ".debug"
+            signingConfig = signingConfigs.getByName("FCLDebugKey")
+        }
+        configureEach {
+            resValue("string", "app_version", defaultConfig.versionName.toString())
+            resValue("string", "curse_api_key", curseApiKey.toString())
+            resValue("string", "oauth_api_key", oauthApiKey.toString())
+        }
+    }
+
+
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+            pickFirsts += listOf("**/libbytehook.so")
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+        resValues = true
+    }
+
+    splits {
+        val arch = System.getProperty("arch", "all")
+        if (arch != "all") {
+            abi {
+                isEnable = true
+                reset()
+                when (arch) {
+                    "arm" -> include("armeabi-v7a")
+                    "arm64" -> include("arm64-v8a")
+                    "x86" -> include("x86")
+                    "x86_64" -> include("x86_64")
+                }
+            }
+        }
+    }
+}
+
+androidComponents {
+    onVariants { variant ->
+        variant.outputs.forEach { output ->
+            if (output is com.android.build.api.variant.impl.VariantOutputImpl) {
+                (output.getFilter(ABI)?.identifier ?: "all").let { abi ->
+                    output.outputFileName =
+                        "FCL-${variant.buildType}-${project.android.defaultConfig.versionName}-${abi}.apk"
+                }
+
+                val variantName = variant.name.replaceFirstChar { it.uppercaseChar() }
+                afterEvaluate {
+                    val task =
+                        tasks.named("merge${variantName}Assets").get() as MergeSourceSetFolders
+                    task.doLast {
+                        val arch = System.getProperty("arch", "all")
+                        val assetsDir = task.outputDir.get().asFile
+                        val jreList = listOf("jre8", "jre17", "jre21", "jre25")
+                        println("arch:$arch")
+                        jreList.forEach { jre ->
+                            val runtimeDir = "$assetsDir/app_runtime/java/$jre"
+                            println("runtimeDir:$runtimeDir")
+                            File(runtimeDir).listFiles().forEach {
+                                if (arch != "all" && it.name != "version" && !it.name.contains("universal") && it.name != "bin-${arch}.tar.xz") {
+                                    println("delete:${it} : ${it.delete()}")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
+    implementation(project(":FCLCore"))
+    implementation(project(":FCLLibrary"))
+    implementation(project(":FCLauncher"))
+    implementation(project(":Terracotta"))
+    implementation(libs.taptargetview)
+    implementation(libs.nanohttpd)
+    implementation(libs.commons.compress)
+    implementation(libs.xz)
+    implementation(libs.opennbt)
+    implementation(libs.gson)
+    implementation(libs.appcompat)
+    implementation(libs.core.splashscreen)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.glide)
+    implementation(libs.touchcontroller)
+    implementation(libs.palette.ktx)
+    implementation(libs.gamepad.remapper)
+    implementation(libs.segmented.button)
+    implementation(libs.datastore)
+    implementation(libs.kotlinx.serialization.json)
+}
+
+tasks.register("updateMap") {
+    doLast {
+        val list = mutableListOf<String>()
+        val mapFile = file("${rootDir}/version_map.json")
+        mapFile.forEachLine {
+            list.add(
+                when {
+                    it.contains("versionCode") -> it.replace(
+                        Regex("[0-9]+"),
+                        android.defaultConfig.versionCode.toString()
+                    )
+
+                    it.contains("versionName") -> it.replace(
+                        Regex("\\d+(\\.\\d+)+"),
+                        android.defaultConfig.versionName.toString()
+                    )
+
+                    it.contains("date") -> it.replace(
+                        Regex("\\d{4}\\.\\d{2}\\.\\d{2}"),
+                        SimpleDateFormat("yyyy.MM.dd").format(Date())
+                    )
+
+                    it.contains("url") -> it.replace(
+                        Regex("\\d+(\\.\\d+)+"),
+                        android.defaultConfig.versionName.toString()
+                    )
+
+                    else -> it
+                }
+            )
+        }
+        mapFile.writeText(list.joinToString("\n"), Charsets.UTF_8)
+    }
+}
